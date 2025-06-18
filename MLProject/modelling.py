70659b5d1fafd10f09ae023c45de15c3053e54da
Xechi <msyuaib776@gmail.com>
2025-06-18 15:22:31 +0800

    Add mlflow nested

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -24,7 +24,7 @@ y = df["Outcome"]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y, random_state=42)
 model = RandomForestClassifier(n_estimators=100, random_state=42)
 
-with mlflow.start_run():
+with mlflow.start_run(nested=True):
     model.fit(X_train, y_train)
     acc = model.score(X_test, y_test)
     mlflow.log_metric("accuracy", acc)
